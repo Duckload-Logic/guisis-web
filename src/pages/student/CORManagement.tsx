2d166df1dd74c639369c66218ce0725291ee0501
gonato-janalexalouissee <alexagonato@gmail.comm>
2026-07-01 22:45:29 +0800

    Revert COR management changes

--- src/pages/student/CORManagement.tsx
+++ src/pages/student/CORManagement.tsx
@@ -144,7 +144,7 @@ export default function CORManagement() {
   return(
       <div
         className={cn(
-        "w-full mx-auto pb-12",
+        "w-full mx-auto pb-12", 
         "px-4 sm:px-6 md:px-8",
         )}
       >
